a4386e1acc83d2e31ac34f6cf4017fb37e5b4f20
TheBloodLess <159092111+TheBloodLess@users.noreply.github.com>
2025-06-09 14:58:46 +0000

    hola

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+#include <figuras2D.c>
+#include <figuras3D.c>
+#include <funciones.h>
+#include <math.h>
+
+int main(){
+    int opcion;
+
+    printf("1.-Cálculo de área y perímetro\n 2.-Cálculo de superficie y volumen");
+    scanf("%d", &opcion);
+
+    if (opcion == 1){
+        
+        
+
+    }
+    return 0;
+}
